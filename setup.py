cba911dd4e7162c5838fc8710448b6599fa646fe
martyfuhry <martyfuhry@gmail.com>
2009-07-12 16:52:47 -0400

    removed some files

--- setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup, Extension
-
-setup(name        = "npy_datetime",
-      version     = "1.0",
-      ext_modules = [Extension("npy_datetime",  ["c_datetime.c"]),
-			         ])
